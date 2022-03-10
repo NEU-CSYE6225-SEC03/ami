c3dcef4bcbfd9f835b8f14d45098a7aab7aa929f
Weifeng Lai <laiweifeng51@gmail.com>
2022-03-10 10:39:58 -0500

    a5

---
+++ sql.sh
@@ -0,0 +1,19 @@
+USE csye6225;
+CREATE TABLE user (
+  id char(50) NOT NULL, Constraint pk_User_id PRIMARY KEY (id),  # 待优化, 不应该拿非线性数据如字符串做索引
+  first_name char(30) NOT NULL,
+  last_name char(30) NOT NULL,
+  username char(50) NOT NULL unique,
+  password char(100) NOT NULL,
+  account_created timestamp NOT NULL,
+  account_updated timestamp NOT NULL
+) default charset=utf8;
+CREATE TABLE image (
+  id char(50) NOT NULL, Constraint pk_image_id PRIMARY KEY (id),  # 待优化, 不应该拿非线性数据如字符串做索引
+  user_id char(50) NOT NULL,
+  url char(100) NOT NULL,
+  file_name char(50) NOT NULL,
+  upload_date timestamp NOT NULL
+) default charset=utf8;
+EOF
+
